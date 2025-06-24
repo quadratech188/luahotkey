ffa58c4c4d872d1b65327e118decd9a6ced88798
Quadratech188 <quadratech188@gmail.com>
2025-06-24 09:20:15 +0900

    feat: Add lhk.criteria.PRESS and lhk.criteria.RELEASE

--- lua/lhk/init.lua
+++ lua/lhk/init.lua
@@ -1,5 +1,17 @@
 local G = require 'lhk_core'
 
+local criteria = G.criteria
+
+criteria.PRESS = G.criteria.new {
+	transition = 'Press'
+}
+
+criteria.RELEASE = G.criteria.new {
+	transition = 'Release'
+}
+
+G.criteria = criteria
+
 G.actions = require 'lhk.actions'
 
 G.util = require 'lhk.util'
